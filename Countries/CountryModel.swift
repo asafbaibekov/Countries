2f854a9027c50037d1db83ada7a5cc37d1cf34e9
asafall <asafall21@gmail.com>
2019-10-16 18:21:47 +0300

    create struct model for country data

---
+++ Countries/CountryModel.swift
@@ -0,0 +1,13 @@
+//
+//  CountryModel.swift
+//  Countries
+//
+//  Created by Asaf Baibekov on 16/10/2019.
+//  Copyright © 2019 Asaf Baibekov. All rights reserved.
+//
+
+import Foundation
+
+struct CountryModel {
+	
+}
