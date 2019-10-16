b830a45943653c8769d2279825170b2b03272de5
asafall <asafall21@gmail.com>
2019-10-17 01:22:38 +0300

    create view model for borders table view

---
+++ Countries/Modules/Borders/ViewModels/BordersViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BordersViewModel.swift
+//  Countries
+//
+//  Created by Asaf Baibekov on 17/10/2019.
+//  Copyright © 2019 Asaf Baibekov. All rights reserved.
+//
+
+import UIKit
+
+class BordersViewModel {
+
+}
