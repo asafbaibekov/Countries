ef41064cffeafeaf9673a8ad367d8ff8b37f6370
asafall <asafall21@gmail.com>
2019-10-16 18:12:33 +0300

    Initial Commit

---
+++ Countries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Asaf Baibekov on 16/10/2019.
+//  Copyright © 2019 Asaf Baibekov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
