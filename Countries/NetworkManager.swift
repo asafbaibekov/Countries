5bc2d4430be6a1e45d630fbbccf97f8012e648d3
asafall <asafall21@gmail.com>
2019-10-16 18:18:06 +0300

    create a network manager as a singleton

---
+++ Countries/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  Countries
+//
+//  Created by Asaf Baibekov on 16/10/2019.
+//  Copyright © 2019 Asaf Baibekov. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+
+	static let shared = NetworkManager()
+
+	private init() {
+		
+	}
+}
