4a23325f00d3776a2fbb079c4d8efacc65237769
asafall <asafall21@gmail.com>
2019-10-16 18:55:36 +0300

    build request fetches all countries from the countries url property

--- Countries/NetworkManager.swift
+++ Countries/NetworkManager.swift
@@ -17,4 +17,21 @@ class NetworkManager {
 	private init() {
 		self.countriesURL = "https://restcountries.eu/rest/v2/all"
 	}
+
+	func fetchCountries(completion: @escaping (Result<[CountryModel], Error>) -> Void) {
+		let url = URL(string: countriesURL)!
+		URLSession.shared.dataTask(with: url) { (data, response, error) in
+			DispatchQueue.main.async {
+				if let error = error {
+					completion(.failure(error))
+				} else if let data = data {
+					do {
+						completion(.success(try JSONDecoder().decode([CountryModel].self, from: data)))
+					} catch {
+						completion(.failure(error))
+					}
+				}
+			}
+		}.resume()
+	}
 }
